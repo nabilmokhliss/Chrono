e73010f019b3d69d7cab454c6e00cb1aec500f8e
nabilmokhliss <mokhlissnabil97@gmail.com>
2023-04-04 22:06:52 +0000

    Chronometre

---
+++ assets/main.js
@@ -0,0 +1,67 @@
+/**
+ * TP Chrono
+ */
+let timerVal = 0;
+let chronoId = document.querySelector('#chronoId');
+let startId = document.querySelector('#startId');
+let stopId = document.querySelector('#stopId');
+let resetId = document.querySelector('#resetId');
+
+
+let heure = 0;
+let minutes = 0;
+let secondes = 0;
+let msecondes = 0;
+
+// Start chorno
+startId.addEventListener('click', startChrono);
+function startChrono() {
+    timerVal = setInterval(() => {
+        // affichage
+        let heureShow = (heure < 10) ? "0" + heure : heure; // ? = si , : = sinon;
+        let minutesShow = (minutes < 10) ? "0" + minutes : minutes;
+        let secondesShow = (secondes < 10) ? "0" + secondes : secondes;
+        let msecondesShow = (msecondes < 10) ? "0" + msecondes : msecondes;
+        chronoId.textContent = heureShow + ' : ' + minutesShow + ' : ' + secondesShow + ' : ' + msecondesShow;
+
+        msecondes += 1;
+        if(msecondes >= 10) {
+            msecondes = 0;
+            secondes += 1;
+        }
+
+        if(secondes >= 60) {
+            secondes = 0;
+            minutes += 1;
+        }
+
+        if(minutes >= 60) {
+            minutes = 0;
+            heure += 1;
+        }
+    }, 100);
+
+    startId.setAttribute('disabled', '');
+}
+
+
+// Stop chrono
+stopId.addEventListener('click', stopChrono);
+function stopChrono() {
+    clearInterval(timerVal);
+
+    startId.removeAttribute('disabled', '');
+};
+
+// Reset chrono
+resetId.addEventListener('click', resetChrono);
+function resetChrono() {
+    heure = 0;
+    minutes = 0;
+    secondes = 0;
+    msecondes = 0;
+    clearInterval(timerVal);
+    chronoId.innerHTML = "00 : 00 : 00 : 00";
+
+    startId.removeAttribute('disabled', '');
+};
